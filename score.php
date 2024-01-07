b64d87bdb1bdf1987b11966e2001ef3ff9404b58
pixis1127 <yumi.kawasaki1127@gmail.com>
2024-01-07 23:56:52 +0900

    5章課題の提出

---
+++ score.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>5章課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+        // 合計
+        $sam = $score1 + $score2 + $score3 + $score4 + $score5 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        echo $sam / 10;
+        ?>
+    </p>
+    
+</body>
+</html>
