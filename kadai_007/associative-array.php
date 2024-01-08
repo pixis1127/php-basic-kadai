0b22117641a7dd64261dadf1b7772ad2cf0c8b99
pixis1127 <150803177+pixis1127@users.noreply.github.com>
2024-01-09 00:49:04 +0900

    7章課題の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>7章課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        $product_date = ['name'=> 'onion', 'price'=> 200, 'weight'=> 160];
+        print_r($product_date);
+        ?>
+    </p> 
+</body>
+</html>
